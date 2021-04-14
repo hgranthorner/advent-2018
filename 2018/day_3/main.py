0377cfdadb98e6cc4f6bb90807f6b1a0a13187bd
Grant Horner <h.grant.horner@gmail.com>
2021-04-14 17:50:27 -0400

    finished day 3

--- 2018/day_3/main.py
+++ 2018/day_3/main.py
@@ -1,20 +1,37 @@
 from dataclasses import dataclass
+from itertools import product
 import re
+from typing import Iterable
+
+# origin is (0,0), increasing down and to the right
+
+
+@dataclass(frozen=True)
+class Point:
+    x: int
+    y: int
 
 
 @dataclass
 class Claim:
     id: int
-    fromLeft: int
-    fromTop: int
+    from_left: int
+    from_top: int
     width: int
     height: int
 
-    def top_left():
-        pass
+    def top_left(self) -> Point:
+        return Point(self.from_left, self.from_top)
 
-    def bottom_right():
-        pass
+    def bottom_left(self) -> Point:
+        return Point(self.from_left, self.from_top + self.height)
+
+    def top_right(self) -> Point:
+        return Point(self.from_left + self.width, self.from_top)
+
+    def bottom_right(self) -> Point:
+        return Point(self.from_left + self.width,
+                     self.from_top + self.height)
 
     @classmethod
     def from_string(cls, string: str):
@@ -23,26 +40,56 @@ class Claim:
         return cls(int(id), int(l), int(t), int(w), int(h))
 
 
-def find_intersection(claim1: Claim, claim2: Claim):
-    pass
-    # x_dist = (min(r1[x], r2[x]) -
-    #           max(l1[x], l2[x]))
+def generate_points(claim: Claim) -> Iterable[Point]:
+    top_left = claim.top_left()
+    min_x, min_y = top_left.x, top_left.y
+    bottom_right = claim.bottom_right()
+    max_x, max_y = bottom_right.x, bottom_right.y
 
-    # y_dist = (min(r1[y], r2[y]) -
-    #           max(l1[y], l2[y]))
-
-
-def part_one():
-    pass
+    point_product = product(range(min_x, max_x), range(min_y, max_y))
+    return [Point(x, y) for (x, y) in point_product]
 
 
-def part_two():
-    pass
+def part_one(lines: list[str]):
+    claims = [Claim.from_string(x) for x in lines]
+    d: dict[Point, int] = {}
+
+    for claim in claims:
+        points = generate_points(claim)
+        upsert_points(d, points)
+
+    return len([v for (_, v) in d.items() if v > 1])
+
+
+def part_two(lines: list[str]):
+    claims = [Claim.from_string(x) for x in lines]
+    d: dict[Point, int] = {}
+    claim_dict: dict[int, list[Point]] = {}
+
+    for claim in claims:
+        points = list(generate_points(claim))
+        claim_dict[claim.id] = points
+        upsert_points(d, points)
+
+    for claim in claims:
+        counts = [d[p] for p in claim_dict[claim.id]]
+        if len([x for x in counts if x > 1]) == 0:
+            return claim.id
+
+
+def upsert_points(d: dict[Point, int], points: Iterable[Point]):
+    for point in points:
+        if point in d:
+            d[point] += 1
+        else:
+            d[point] = 1
 
 
 if __name__ == '__main__':
-    with open('input.txt') as f:
-        lines = f.readlines()
-        for line in lines:
-            claim = Claim.from_string(line)
-            print(claim)
+    file_name = 'input.txt'
+    try:
+        with open(file_name) as f:
+            print(part_two(f.readlines()))
+
+    except FileNotFoundError:
+        print(f'{file_name} not found')
